fa21266f3cde141accccf1c5a59f071a07874c6c
Austin Carlile <123428041+Snow0611@users.noreply.github.com>
2024-12-03 21:40:43 -0700

    updated microservices

--- CarDealership/Program.cs
+++ CarDealership/Program.cs
@@ -4,20 +4,13 @@ using CarDealershipAPI.Models;
 // Setting up configuration for the web api
 var builder = WebApplication.CreateBuilder(args);
 
-// Adding package references to the model classes
-builder.Services.AddScoped<CarFunctions>();
-builder.Services.AddScoped<StaffFunctions>();
-
 // Adding controller and view support
 builder.Services.AddControllersWithViews();
 
+// Creating HTTP clients to connect to the Car, Staff and Lot Microservices
 builder.Services.AddHttpClient("CarClient", httpClient => { httpClient.BaseAddress = new Uri("https://localhost:7180"); });
 builder.Services.AddHttpClient("StaffClient", httpClient => { httpClient.BaseAddress = new Uri("https://localhost:7181"); });
 builder.Services.AddHttpClient("LotClient", httpClient => { httpClient.BaseAddress = new Uri("https://localhost:7182"); });
-
-// Initializing DatabaseContext object with 'option' paramaters, connecting to the SQL server
-// using connection string from appsettings.json, then adding DatabaseContext to the web api
-builder.Services.AddDbContext<DatabaseContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("Connection"), option => option.EnableRetryOnFailure()));
 
 // Building the web api
 var app = builder.Build();
